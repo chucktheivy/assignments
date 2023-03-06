224b87376d5441a2fca1a01b00d987ddedddcaf7
chucktheivy <charliewilson@ucsb.edu>
2023-03-06 10:50:27 -0800

    Hi

---
+++ fishcount.R
@@ -0,0 +1,16 @@
+fishsummary = function(fishVec) {
+  fishFact = as.factor(fishVec)
+
+  # max name and number
+  a = names(which.max(summary(fishFact)))
+  b = max(summary(fishFact))
+
+  # min name and number
+  c = names(which.min(summary(fishFact)))
+  d = min(summary(fishFact))
+
+  # calculate total number of fish
+  e = summary(fishVec)
+
+  return(a)
+}
